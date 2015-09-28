476ba23f0ac2c428aabe7f6ef884825824dbacbc
Jordan Lazo <raji_srl@outlook.com>
2015-09-27 21:18:07 -0500

    modificaciones: muestra la hora en que ingresa

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
 from app import app
+from flask import Flask, render_template, redirect, request
+import time
+
+app = Flask(__name__, template_folder='app/templates')
+
+@app.route("/")
+def index():
+	# hour = time.strftime("%H:%M:%S")
+	return render_template(
+		'index.html', hour = time.strftime("%H:%M:%S")
+		)
+
 app.run(host='0.0.0.0', port=8080, debug=True)
